18a428e3155db240d805b02cb3f9fe65d7fbbb10
Spark <kelian.barry@outlook.fr>
2021-12-22 17:55:00 +0100

    push du travis

---
+++ specs/employee.spec
@@ -0,0 +1,20 @@
+Specification Heading
+=====================
+Created by kelian on 23/11/2021
+
+This is an executable specification file which follows markdown syntax.
+Every heading in this file denotes a scenario. Every bulleted point denotes a step.
+     
+Nombre d'année d'ancienneté d'un employé embauché au cours de l'année
+----------------
+
+* Soit un employé embauché cette année
+* Lorque je calcule le nombre d'année d'ancienneté
+* J'obtiens "0"
+
+Nombre d'année d'ancienneté d'un employé embauché l'année dernière
+----------------
+
+* Soit un employé embauché il y a "1" an
+* Lorque je calcule le nombre d'année d'ancienneté
+* J'obtiens "1"
---
+++ src/test/java/StepImplementation.java
@@ -0,0 +1,69 @@
+import com.ipiecoles.java.java350.model.Employe;
+import com.thoughtworks.gauge.Step;
+import com.thoughtworks.gauge.Table;
+import com.thoughtworks.gauge.TableRow;
+import org.assertj.core.api.Assertions;
+
+import java.awt.print.PrinterGraphics;
+import java.time.LocalDate;
+import java.util.HashSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StepImplementation {
+
+    private HashSet<Character> vowels;
+
+    @Step("Vowels in English language are <vowelString>.")
+    public void setLanguageVowels(String vowelString) {
+        vowels = new HashSet<>();
+        for (char ch : vowelString.toCharArray()) {
+            vowels.add(ch);
+        }
+    }
+
+    @Step("The word <word> has <expectedCount> vowels.")
+    public void verifyVowelsCountInWord(String word, int expectedCount) {
+        int actualCount = countVowels(word);
+        assertThat(expectedCount).isEqualTo(actualCount);
+    }
+
+    @Step("Almost all words have vowels <wordsTable>")
+    public void verifyVowelsCountInMultipleWords(Table wordsTable) {
+        for (TableRow row : wordsTable.getTableRows()) {
+            String word = row.getCell("Word");
+            int expectedCount = Integer.parseInt(row.getCell("Vowel Count"));
+            int actualCount = countVowels(word);
+
+            assertThat(expectedCount).isEqualTo(actualCount);
+        }
+    }
+
+    private int countVowels(String word) {
+        int count = 0;
+        for (char ch : word.toCharArray()) {
+            if (vowels.contains(ch)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    Employe employe;
+    Integer nbAnneesAnciennete;
+
+    @Step("Soit un employé embauché cette année")
+    public void createEmploye() {
+        Employe employe = new Employe("Keanu", "Reeves", "M12345", LocalDate.now(), 2500d, 1, 1.0);
+    }
+
+    @Step("Lorque je calcule le nombre d'année d'ancienneté")
+    public void getNbAnneeAnciennete() {
+        nbAnneesAnciennete = employe.getNombreAnneeAnciennete();
+    }
+
+    @Step("J'obtiens <ancienneteCalculee>")
+    public void verifAnciennete(Integer ancienneteCalculee) {
+        Assertions.assertThat(nbAnneesAnciennete).isEqualTo(ancienneteCalculee);
+    }
+}
